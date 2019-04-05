29d5f62d9be5cdf698b4cab4d58a14117f0369ba
Amrith <arjun.amrith@gmail.com>
2019-04-05 23:46:40 +0530

    trying some stuff out

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,7 +4,7 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',//automatically (line 6)updates title to what ever the value of variable title is below
     template: `
-    <h2>{{ getTitle() }}</h2>
+    <h2> {{ getTitle() }}</h2>
      <ul>
         <li *ngFor="let course of courses">
             {{ course }}
@@ -16,14 +16,15 @@ import { CoursesService } from './courses.service';
 
 
 export class CoursesComponent{
-    title="List of Courses";
-    courses:any;
+    title="Courses";
+    courses;
     constructor(service: CoursesService){//when using this in the constructor declaration please remember to update providers in app.module
         // let service= new CoursesService();(this is not preffered instead we do the above in the constructor declaration)
         this.courses = service.getCourses();
     }
     getTitle(){
-        return this.title;
+        let x=this.courses.lastIndexOf()+1;
+        return (x as string)+this.title;
     }
     //logic for calling HTTP endpoint service
 
