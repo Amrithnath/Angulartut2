19237d74b35730398da0f3e08e1b9e95e67b2f17
Amrith <arjun.amrith@gmail.com>
2019-04-05 23:20:03 +0530

    added some string interpolation and what not

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
-
+    template: `
+    <h2>{{ getTitle() }}</h2>
+     <ul>
+        <li *ngFor=""></li>
+    </ul>                  
+    `
+    //string interpolation
+    //automatically updates title to what ever the value of variable title is below
 })
 
 
 export class CoursesComponent{
+    title="List of Courses";
+    courses = ["math","Physics","English"];
+    getTitle(){
+        return this.title;
+    }
     
 }
